5c408603c82b36a043943549daeb3549d6dde975
Anton Yaskov <yaskovanton333@gmail.com>
2023-11-11 15:40:24 +0500

    База под виджет

---
+++ API/API/Modules/VidjetsModule/VidjetsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Modules.VidjetsModule;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VidjetsController : ControllerBase
+{
+    
+}
---
+++ API/API/Modules/VidjetsModule/VidjetsModule.cs
@@ -0,0 +1,17 @@
+﻿using API.Infrastructure;
+
+namespace API.Modules.VidjetsModule;
+
+public class VidjetsModule : IModule
+{
+    public IServiceCollection RegisterModule(IServiceCollection services)
+    {
+        
+
+        return services;
+    }
+
+    public void ConfigureHubs(WebApplication app)
+    {
+    }
+}
