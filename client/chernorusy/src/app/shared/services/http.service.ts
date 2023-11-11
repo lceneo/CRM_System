2d08faf394c4c9a8c6406920589fec330660866e
Iceneo <nosminin@rit-it.com>
2023-11-11 21:00:16 +0500

    http link changed

--- client/chernorusy/src/app/shared/services/http.service.ts
+++ client/chernorusy/src/app/shared/services/http.service.ts
@@ -5,30 +5,28 @@ import {HttpClient, HttpHeaders } from "@angular/common/http";
   providedIn: 'root'
 })
 export class HttpService {
-
-  private url = 'localhost:4200';
   constructor(
     private httpClient: HttpClient
   ) {}
 
   public get<T>(method: string) {
-    return this.httpClient.get<T>(`https://${this.url}/api${method}`);
+    return this.httpClient.get<T>(`/api${method}`);
   }
 
   public post<T>(method: string, body: any, options?: { headers: HttpHeaders | {[p: string] : string} }) {
-    return this.httpClient.post<T>(`https://${this.url}/api${method}`, body, options);
+    return this.httpClient.post<T>(`/api${method}`, body, options);
   }
 
   public put(method: string, body: any, options?: { headers: HttpHeaders | {[p: string] : string} }) {
-    return this.httpClient.put(`https://${this.url}/api${method}`, body, options);
+    return this.httpClient.put(`/api${method}`, body, options);
   }
 
   public patch(method: string, body: any, options?: { headers: HttpHeaders | {[p: string] : string} }) {
-    return this.httpClient.patch(`https://${this.url}/api${method}`, body, options);
+    return this.httpClient.patch(`/api${method}`, body, options);
   }
 
   public delete(method: string, options?: { headers: HttpHeaders | {[p: string] : string} }) {
-    return this.httpClient.delete(`https://${this.url}/api${method}`, options);
+    return this.httpClient.delete(`/api${method}`, options);
   }
 
 }
