e47fd93ce81c17efad431e6c3a7fe35b24ca43af
Anton Yaskov <yaskovanton333@gmail.com>
2023-12-02 15:02:35 +0500

    В каком-то виде

---
+++ API/API/Modules/VidjetsModule/Ports/IVidjetsService.cs
@@ -0,0 +1,18 @@
+﻿using API.Infrastructure;
+using API.Infrastructure.BaseApiDTOs;
+using API.Modules.VidjetsModule.DTO;
+using API.Modules.VidjetsModule.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Modules.VidjetsModule.Ports;
+
+public interface IVidjetsService
+{
+    Task<Result<IEnumerable<VidjetOutDTO>>> GetVidjetsAsync();
+    
+    Task<Result<VidjetOutDTO>> GetVidjetByIdAsync([FromRoute] Guid vidjetId);
+    
+    Task<Result<CreateResponse>> CreateOrUpdateVidjet(VidjetCreateRequest vidjetCreateRequest);
+
+    Task<ActionResult> DeleteVidjetAsync([FromRoute] Guid vidjetId);
+}
--- API/API/Modules/VidjetsModule/VidjetsController.cs
+++ API/API/Modules/VidjetsModule/VidjetsController.cs
@@ -1,10 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Modules.VidjetsModule.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Modules.VidjetsModule;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class VidjetsController : ControllerBase
 {
+
+    [HttpGet]
+    public async Task<ActionResult> GetVidjetsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{vidjetId:Guid}")]
+    public async Task<ActionResult> GetVidjetByIdAsync([FromRoute] Guid vidjetId)
+    {
+        throw new NotImplementedException();
+    }
     
+    [HttpPost]
+    public async Task<ActionResult> CreateOrUpdateVidjet(VidjetCreateRequest vidjetCreateRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{vidjetId:Guid}")]
+    public async Task<ActionResult> DeleteVidjetAsync([FromRoute] Guid vidjetId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [AllowAnonymous]
+    [HttpPost("VidjetInfo")]
+    public async Task<ActionResult<VidjetResponse>> GetTokenAsync(VidjetRequest request)
+    {
+        var ip = HttpContext.Connection.RemoteIpAddress;
+        if (ip == null)
+            return BadRequest("IP is required");
+
+        return new VidjetResponse
+        {
+            
+        };
+    }
 }
