4f95777bd2003d49d66cdc39e430e8155ff93c48
Anton Yaskov <yaskovanton333@gmail.com>
2023-12-21 20:09:20 +0500

    Чистки

--- API/API/Infrastructure/Middlewares/ExceptionHandlerMiddleware.cs
+++ API/API/Infrastructure/Middlewares/ExceptionHandlerMiddleware.cs
@@ -33,34 +33,5 @@ public class ExceptionHandlerMiddleware
 
         log.Error($"Route: {context.Request.Path} Exception: {exception.Message}");
         return context.Response.WriteAsync(JsonSerializer.Serialize(exception.Message));  
-    } 
-    
-    /*public static void Register(WebApplication app)
-    {
-        app.UseExceptionHandler(exceptionHandlerApp =>
-        {
-            exceptionHandlerApp.Run(async context =>
-            {
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-
-                // using static System.Net.Mime.MediaTypeNames;
-                context.Response.ContentType = MediaTypeNames.Text.Plain;
-
-                await context.Response.WriteAsync("An exception was thrown.");
-
-                var exceptionHandlerPathFeature =
-                    context.Features.Get<>();
-
-                if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
-                {
-                    await context.Response.WriteAsync(" The file was not found.");
-                }
-
-                if (exceptionHandlerPathFeature?.Path == "/")
-                {
-                    await context.Response.WriteAsync(" Page: Home.");
-                }
-            });
-        });
-    }*/
+    }
 }
