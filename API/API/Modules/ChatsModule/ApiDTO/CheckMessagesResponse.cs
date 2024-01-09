a0eeb58ce27b8fb604333e2c77eec31ec3d22c71
Anton Yaskov <yaskovanton333@gmail.com>
2024-01-09 21:41:46 +0500

    Ещё доделки

---
+++ API/API/Modules/ChatsModule/ApiDTO/CheckMessagesResponse.cs
@@ -0,0 +1,10 @@
+﻿using API.Modules.ProfilesModule.DTO;
+
+namespace API.Modules.ChatsModule.ApiDTO;
+
+public class CheckMessagesResponse
+{
+    public ProfileOutShortDTO Checker { get; set; }
+    public Guid ChatId { get; set; }
+    public IEnumerable<Guid> MessageIds { get; set; }
+}
