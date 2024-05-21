eb6773777fa768a42ecf1b592c2e930052215725
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2024-05-21 15:33:49 +0500

    Добавлен пакет для графиков и новые методы для статистики

--- client/chernorusy/src/app/modules/diagrams/components/diagrams/modules/received-dialogs-overall/received-dialogs-overall.component.ts
+++ client/chernorusy/src/app/modules/diagrams/components/diagrams/modules/received-dialogs-overall/received-dialogs-overall.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-received-dialogs-overall',
   templateUrl: './received-dialogs-overall.component.html',
-  styleUrls: ['./received-dialogs-overall.component.scss']
+  styleUrls: ['./received-dialogs-overall.component.scss'],
 })
-export class ReceivedDialogsOverallComponent {
-
-}
+export class ReceivedDialogsOverallComponent {}
--- client/chernorusy/src/app/shared/services/statistics.service.ts
+++ client/chernorusy/src/app/shared/services/statistics.service.ts
@@ -1,43 +1,100 @@
-import {Injectable} from "@angular/core";
-import {HttpService} from "./http.service";
-import {catchError, tap} from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { catchError, tap } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class StatisticsService {
-  constructor(
-    private httpS: HttpService,
-  ) {}
+  constructor(private httpS: HttpService) {}
 
-  getAverageAnswerTime$(managerIds: string[], startTimestamp: Date | string | number, endTimestamp: Date | string | number) {
-    startTimestamp = new Date(startTimestamp).toISOString();
-    endTimestamp = new Date(endTimestamp).toISOString();
-    return this.httpS.post<GetAverageAnswerTimeResponse[]>('/Statistics/AverageAnswerTime', {
-      startTime: startTimestamp,
-      endTime: endTimestamp,
-      managerIds: managerIds
-    }).pipe(
-      catchError(() => []),
-    )
+  getAverageAnswerTime$(
+    managerIds: string[],
+    startTimestamp: Date | string | number,
+    endTimestamp: Date | string | number
+  ) {
+    [startTimestamp, endTimestamp] = this.prependDates(
+      startTimestamp,
+      endTimestamp
+    );
+    return this.httpS
+      .post<GetAverageAnswerTimeResponse[]>('/Statistics/AverageAnswerTime', {
+        startTime: startTimestamp,
+        endTime: endTimestamp,
+        managerIds: managerIds,
+      })
+      .pipe(catchError(() => []));
+  }
+
+  getActivityStats$(
+    managerIds: string[],
+    startTimestamp: Date | string | number,
+    endTimestamp: Date | string | number
+  ) {
+    [startTimestamp, endTimestamp] = this.prependDates(
+      startTimestamp,
+      endTimestamp
+    );
+    return this.httpS.post<GetActivityStatsResponse[]>(
+      '/Statistics/ActivityStats',
+      {
+        managerIds,
+        startTime: startTimestamp,
+        endTime: endTimestamp,
+      }
+    );
+  }
+
+  getFirstMessageAverageAnswerTime(
+    managerIds: string[],
+    startTimestamp: Date | string | number,
+    endTimestamp: Date | string | number
+  ) {
+    [startTimestamp, endTimestamp] = this.prependDates(
+      startTimestamp,
+      endTimestamp
+    );
+    return this.httpS.post<GetAverageAnswerTimeResponse[]>(
+      '/Statistics/FirstMessageAverageAnswerTime',
+      {
+        managerIds,
+        startTime: startTimestamp,
+        endTime: endTimestamp,
+      }
+    );
+  }
+
+  private prependDates(
+    startTimestamp: Date | string | number,
+    endTimestamp: Date | string | number
+  ): [string, string] {
+    return [startTimestamp, endTimestamp].map((d) =>
+      new Date(d).toISOString()
+    ) as [string, string];
   }
 }
 
 export interface GetAverageAnswerTimeResponse {
-  "managerId": string,
-  "averageTime": {
-    "ticks": number,
-    "days": number,
-    "hours": number,
-    "milliseconds": number,
-    "microseconds": number,
-    "nanoseconds": number,
-    "minutes": number,
-    "seconds": number,
-    "totalDays": number,
-    "totalHours": number,
-    "totalMilliseconds": number,
-    "totalMicroseconds": number,
-    "totalNanoseconds": number,
-    "totalMinutes": number,
-    "totalSeconds": number
-  } | null
+  managerId: string;
+  averageTime: {
+    ticks: number;
+    days: number;
+    hours: number;
+    milliseconds: number;
+    microseconds: number;
+    nanoseconds: number;
+    minutes: number;
+    seconds: number;
+    totalDays: number;
+    totalHours: number;
+    totalMilliseconds: number;
+    totalMicroseconds: number;
+    totalNanoseconds: number;
+    totalMinutes: number;
+    totalSeconds: number;
+  } | null;
+}
+
+export interface GetActivityStatsResponse {
+  managerId: string;
+  dailyStat: { date: string; dialogsCount: number; messagesCount: number }[];
+  totalMessagesCount: number;
 }
