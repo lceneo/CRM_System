ca301c024df4dd4ee642439c91efae62a3ef660c
Anton Yaskov <yaskovanton333@gmail.com>
2024-05-31 19:59:59 +0500

    Фикс маппинга продуктов

--- API/API/Modules/ProductsModule/ProductsMapping.cs
+++ API/API/Modules/ProductsModule/ProductsMapping.cs
@@ -11,7 +11,7 @@ public class ProductsMapping : Profile
         CreateMap<CreateOrUpdateProductRequest, ProductEntity>()
             .ForAllMembers(opt => opt.Condition((src, dest, curSrc) => curSrc != null));
         CreateMap<ProductEntity, ProductFullDTO>()
-            .ForMember(dest => dest.TaskIds, opt => opt.MapFrom(src => src.Tasks));
+            .ForMember(dest => dest.TaskIds, opt => opt.MapFrom(src => src.Tasks.Select(t => t.Id)));
         CreateMap<ProductEntity, ProductDTO>();
         CreateMap<ProductEntity, ProductEntity>();
     }
